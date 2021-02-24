cada63e70ed0f2e9d79470c8b77b035b38700c1e
EwelinaStasiak <ewelina.stasiak5@gmail.com>
2021-02-24 11:11:14 +0100

    modal with user registration form

--- data_handler.py
+++ data_handler.py
@@ -1,34 +1,34 @@
-import Persistence
+# import Persistence
 import connection
 from psycopg2.extras import RealDictCursor, DictCursor
 import bcrypt
 
-def get_card_status(status_id):
- """
- Find the first status matching the given id
- :param status_id:
- :return: str
- """
- statuses = Persistence.get_statuses()
- return next((status['title'] for status in statuses if status['id'] == str(status_id)), 'Unknown')
-
-
-# def get_boards():
+# def get_card_status(status_id):
 #  """
-#  Gather all boards
-#  :return:
+#  Find the first status matching the given id
+#  :param status_id:
+#  :return: str
 #  """
-#  return Persistence.get_boards(force=True)
-
-def get_cards_for_board(board_id):
- Persistence.clear_cache()
- all_cards = Persistence.get_cards()
- matching_cards = []
- for card in all_cards:
-     if card['board_id'] == str(board_id):
-         card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
-         matching_cards.append(card)
- return matching_cards
+#  statuses = Persistence.get_statuses()
+#  return next((status['title'] for status in statuses if status['id'] == str(status_id)), 'Unknown')
+#
+#
+# # def get_boards():
+# #  """
+# #  Gather all boards
+# #  :return:
+# #  """
+# #  return Persistence.get_boards(force=True)
+#
+# def get_cards_for_board(board_id):
+#  Persistence.clear_cache()
+#  all_cards = Persistence.get_cards()
+#  matching_cards = []
+#  for card in all_cards:
+#      if card['board_id'] == str(board_id):
+#          card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
+#          matching_cards.append(card)
+#  return matching_cards
 
 
 @connection.connection_handler
