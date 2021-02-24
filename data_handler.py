fcf43c68e54c5c34912276102cc50b81defd2a0a
jzakrzewska <askazakrzewska@gmail.com>
2021-02-24 11:38:22 +0100

    flask_login data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -29,6 +29,37 @@ import bcrypt
 #          card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
 #          matching_cards.append(card)
 #  return matching_cards
+class User(object):
+    """An admin user capable of viewing reports.
+
+    :param str login: email address of user
+    :param str password: encrypted password for the user
+
+    """
+    #__tablename__ = 'user'
+
+    def __init__(self,user):
+        self.id = user['id']
+        self.userName = user['userName']
+        self.password = user['password']
+        self.authenticated = True
+
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.id
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
 
 
 @connection.connection_handler
